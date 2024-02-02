8be2d3b9eed30dea8c429e17e573a157802a37fc
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2024-02-02 15:43:23 -0300

    feat(contents): add basic structure of serializers

---
+++ apps/contents/serializers.py
@@ -0,0 +1,65 @@
+"""Serializers for Contents App."""
+
+from rest_framework import serializers
+from apps.contents.models import (
+    Url,
+    Studio,
+    Genre,
+    Premiered,
+    Rating,
+    Content,
+)
+
+
+class UrlSerializer(serializers.ModelSerializer):
+    """Serializer for Url model."""
+
+    class Meta:
+        """Meta definition for UrlSerializer."""
+        model = Url
+        fields = '__all__'
+
+
+class StudioSerializer(serializers.ModelSerializer):
+    """Serializer for Studio model."""
+
+    class Meta:
+        """Meta definition for StudioSerializer."""
+        model = Studio
+        fields = '__all__'
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    """Serializer for Genre model."""
+
+    class Meta:
+        """Meta definition for GenreSerializer."""
+        model = Genre
+        fields = '__all__'
+
+
+class PremieredSerializer(serializers.ModelSerializer):
+    """Serializer for Premiered model."""
+
+    class Meta:
+        """Meta definition for PremieredSerializer."""
+        model = Premiered
+        fields = '__all__'
+
+
+class RatingSerializer(serializers.ModelSerializer):
+    """Serializer for Rating model."""
+
+    class Meta:
+        """Meta definition for RatingSerializer."""
+        model = Rating
+        fields = '__all__'
+
+
+class ContentSerializer(serializers.ModelSerializer):
+    """Serializer for Content model."""
+
+    class Meta:
+        """Meta definition for ContentSerializer."""
+        model = Content
+        fields = '__all__'
