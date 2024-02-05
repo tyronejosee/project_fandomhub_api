967443bbf7ed6737b805527eb5ff57252077ba25
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2024-02-05 09:50:42 -0300

    feat(utils): global throttles

---
+++ apps/utils/throttles.py
@@ -0,0 +1,24 @@
+"""Throttles for Utils App."""
+
+from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
+
+
+class AnonUserThrottle(AnonRateThrottle):
+    """Throttle class for anon users (100 requests/day)."""
+    scope = 'anon'
+    THROTTLE_RATES = {'anon': '100/day',}
+
+
+class AuthUserThrottle(UserRateThrottle):
+    """Throttle class for auth users (1000 requests/day)."""
+    scope = 'user'
+    THROTTLE_RATES = {'user': '1000/day',}
+
+
+class StaffUserThrottle(UserRateThrottle):
+    """Throttle class for staff (no limits)."""
+    scope = 'staff'
+    THROTTLE_RATES = {'staff': None,}
+
+    def allow_request(self, request, view):
+        return request.user and request.user.is_staff or super().allow_request(request, view)
