306d94d8abf8e82f1b5068925c161edc561c30fc
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2024-02-12 15:47:57 -0300

    feat(watchlists): add admin configs

--- apps/watchlists/apps.py
+++ apps/watchlists/apps.py
@@ -1,6 +1,9 @@
+"""Configs for Watchlists App."""
+
 from django.apps import AppConfig
 
 
 class WatchlistsConfig(AppConfig):
+    """Configs for Watchlists App."""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.watchlists'
