078b01802fd037f147e71d81c8149cab14c0c5fe
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2024-02-19 19:03:18 -0300

    refactor(users): separate logic of managers and models

---
+++ apps/users/managers.py
@@ -0,0 +1,37 @@
+"""Managers for Users App."""
+
+from django.contrib.auth.models import BaseUserManager
+from django.utils.translation import gettext as _
+from apps.profiles.models import Profile
+
+
+class UserManager(BaseUserManager):
+    """Manager for User instances."""
+
+    def create_user(self, email, password=None, **kwargs):
+        """Creates and returns a user with the given email and password."""
+        if not email:
+            raise ValueError(_("Users must have an email address"))
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **kwargs)
+
+        user.set_password(password)
+        user.save()
+
+        profile = Profile.objects.create(user=user)
+        profile.save()
+        return user
+
+    def create_superuser(self, email, password, **kwargs):
+        """Creates a superuser with the given email and password."""
+        kwargs.setdefault("is_staff", True)
+        kwargs.setdefault("is_superuser", True)
+
+        if kwargs.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        if kwargs.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
+
+        user = self.create_user(email, password=password, **kwargs)
+        return user
--- apps/users/models.py
+++ apps/users/models.py
@@ -2,44 +2,10 @@
 
 import uuid
 from django.db import models
-from django.contrib.auth.models import (
-    BaseUserManager, AbstractBaseUser, PermissionsMixin
-)
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.utils.translation import gettext as _
 from django.utils import timezone
-from apps.profiles.models import Profile
-
-
-class UserManager(BaseUserManager):
-    """Manages User instances."""
-
-    def create_user(self, email, password=None, **kwargs):
-        """Creates and returns a user with the given email and password."""
-        if not email:
-            raise ValueError(_("Users must have an email address"))
-
-        email = self.normalize_email(email)
-        user = self.model(email=email, **kwargs)
-
-        user.set_password(password)
-        user.save()
-
-        profile = Profile.objects.create(user=user)
-        profile.save()
-        return user
-
-    def create_superuser(self, email, password, **kwargs):
-        """Creates a superuser with the given email and password."""
-        kwargs.setdefault("is_staff", True)
-        kwargs.setdefault("is_superuser", True)
-
-        if kwargs.get("is_staff") is not True:
-            raise ValueError(_("Superuser must have is_staff=True."))
-        if kwargs.get("is_superuser") is not True:
-            raise ValueError(_("Superuser must have is_superuser=True."))
-
-        user = self.create_user(email, password=password, **kwargs)
-        return user
+from apps.users.managers import UserManager
 
 
 class Role(models.Model):
