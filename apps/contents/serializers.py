7b657ddb23fadc726f5339aeb556c36fb02e036e
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2024-03-13 17:03:25 -0300

    feat(contents): add minimunserializers for anime and manga models

--- apps/contents/serializers.py
+++ apps/contents/serializers.py
@@ -42,6 +42,17 @@ class AnimeListSerializer(serializers.ModelSerializer):
         return obj.season.year if obj.season else None
 
 
+class AnimeMinimumSerializer(serializers.ModelSerializer):
+    """Serializer for Anime model (Minimum)."""
+
+    class Meta:
+        """Meta definition for AnimeListSerializer."""
+        model = Anime
+        fields = [
+            "id", "name", "image"
+        ]
+
+
 class MangaSerializer(serializers.ModelSerializer):
     """Serializer for Manga model."""
     author = serializers.CharField(source="author.name")
@@ -66,4 +77,15 @@ class MangaListSerializer(serializers.ModelSerializer):
         model = Manga
         fields = [
             "id", "name", "image", "release", "media_type", "status"
+        ]
+
+
+class MangaMinimumSerializer(serializers.ModelSerializer):
+    """Serializer for Anime model (Minimum)."""
+
+    class Meta:
+        """Meta definition for AnimeListSerializer."""
+        model = Manga
+        fields = [
+            "id", "name", "image"
         ]
