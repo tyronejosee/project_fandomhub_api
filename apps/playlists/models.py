3b7f389d17bd52183156a2bcfd5565b8ac1a8a25
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2024-03-13 17:09:43 -0300

    refactor(playlists): update playlist and playlistitem models

--- apps/playlists/models.py
+++ apps/playlists/models.py
@@ -3,7 +3,6 @@
 from django.conf import settings
 from django.db import models
 from django.utils.translation import gettext as _
-from django.core.validators import MinValueValidator, MaxValueValidator
 from apps.contents.models import Anime, Manga
 from apps.playlists.choices import STATUS_CHOICES
 
@@ -11,13 +10,14 @@ User = settings.AUTH_USER_MODEL
 
 
 class Playlist(models.Model):
-    """Pending."""
-
-    user = models.OneToOneField(
+    """Model definition for Playlist (Entity)."""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
         User, on_delete=models.CASCADE, db_index=True, verbose_name=_("User")
     )
-    name = models.CharField(max_length=255)
-    animes = models.ManyToManyField(Anime, through="PlaylistAnime")
+    created_at = models.DateTimeField(auto_now_add=True)
+    anime_items = models.ManyToManyField(Anime, through="PlaylistItem")
+    manga_items = models.ManyToManyField(Manga, through="PlaylistItem")
 
     class Meta:
         """Meta definition for Playlist."""
@@ -25,62 +25,26 @@ class Playlist(models.Model):
         verbose_name_plural = _("Playlists")
 
     def __str__(self):
-        return str(self.user)
+        return str(f"{self.user}'s playlist - {self.name}")
 
 
-class PlaylistAnime(models.Model):
-    """Pending."""
-
+class PlaylistItem(models.Model):
+    """Model definition for PlaylistItem (Pivot)."""
     playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE)
-    anime = models.ForeignKey(Anime, on_delete=models.CASCADE)
-    rating = models.PositiveSmallIntegerField(
-        default=0, validators=[MinValueValidator(1), MaxValueValidator(10)]
-    )
+    anime = models.ForeignKey(
+        Anime, on_delete=models.CASCADE, null=True, blank=True)
+    manga = models.ForeignKey(
+        Manga, on_delete=models.CASCADE, null=True, blank=True)
     status = models.CharField(
-        _("Status"), max_length=20, choices=STATUS_CHOICES,
+        max_length=20, choices=STATUS_CHOICES,
         default="pending", db_index=True
     )
-    is_watched = models.BooleanField(
-        _("Is Watched"), default=False, db_index=True
-    )
-    score = models.IntegerField(_("Score"), default=0)  # 10
-    priority = models.IntegerField(_("Priority"), default=0)
-    tags = models.CharField(_("Tags"), max_length=255, blank=True)
-    comments = models.TextField(_("Comments"), blank=True)
+    is_watched = models.BooleanField(default=False, db_index=True)
 
     class Meta:
-        """Meta definition for PlaylistAnime."""
-        verbose_name = _("PlaylistAnime")
-        verbose_name_plural = _("PlaylistAnime")
+        """Meta definition for PlaylistItem."""
+        verbose_name = _("PlaylistItem")
+        verbose_name_plural = _("PlaylistItems")
 
     def __str__(self):
-        return str(f"{self.playlist} - {self.anime}")
-
-
-class PlaylistManga(models.Model):
-    """Pending."""
-
-    playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE)
-    manga = models.ForeignKey(Manga, on_delete=models.CASCADE)
-    rating = models.PositiveSmallIntegerField(
-        default=0, validators=[MinValueValidator(1), MaxValueValidator(10)]
-    )
-    status = models.CharField(
-        _("Status"), max_length=20, choices=STATUS_CHOICES,
-        default="pending", db_index=True
-    )
-    is_watched = models.BooleanField(
-        _("Is Watched"), default=False, db_index=True
-    )
-    score = models.IntegerField(_("Score"), default=0)  # 10
-    priority = models.IntegerField(_("Priority"), default=0)
-    tags = models.CharField(_("Tags"), max_length=255, blank=True)
-    comments = models.TextField(_("Comments"), blank=True)
-
-    class Meta:
-        """Meta definition for PlaylistManga."""
-        verbose_name = _("PlaylistManga")
-        verbose_name_plural = _("PlaylistManga")
-
-    def __str__(self):
-        return str(f"{self.playlist} - {self.manga}")
+        return str(f"{self.playlist} items")
