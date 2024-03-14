703876ab80e58171e83c4098785b211434deb15e
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2024-03-14 18:37:34 -0300

    refactor(playlists): update serializers

--- apps/playlists/serializers.py
+++ apps/playlists/serializers.py
@@ -1,7 +1,7 @@
 """Serializers for Playlists App."""
 
 from rest_framework import serializers
-from apps.playlists.models import Playlist, PlaylistItem
+from apps.playlists.models import Playlist, PlaylistAnime
 from apps.contents.serializers import (
     AnimeMinimumSerializer, MangaMinimumSerializer
 )
@@ -11,21 +11,32 @@ class PlaylistSerializer(serializers.ModelSerializer):
     """Serializer for Playlist model."""
 
     class Meta:
-        """Pending."""
+        """Meta definition for PlaylistSerializer."""
         model = Playlist
         fields = [
-            "id", "name", "user", "created_at", "anime_items", "manga_items"
+            "id", "name", "created_at"
         ]
 
 
-class PlaylistItemSerializer(serializers.ModelSerializer):
-    """Serializer for PlaylistItem model."""
+class PlaylistAnimeSerializer(serializers.ModelSerializer):
+    """Serializer for PlaylistAnime model."""
     anime = AnimeMinimumSerializer()
+
+    class Meta:
+        """Meta definition for PlaylistAnimeSerializer."""
+        model = PlaylistAnime
+        fields = [
+            "id", "playlist", "anime", "status", "is_watched", "is_favorite"
+        ]    # Add order field
+
+
+class PlaylistMangaSerializer(serializers.ModelSerializer):
+    """Serializer for PlaylistManga model."""
     manga = MangaMinimumSerializer()
 
     class Meta:
-        """Pending."""
-        model = PlaylistItem
+        """Meta definition for PlaylistMangaSerializer."""
+        model = PlaylistAnime
         fields = [
-            "id", "playlist", "anime", "manga", "status", "is_watched"
-        ]
+            "id", "playlist", "manga", "status", "is_watched", "is_favorite"
+        ]   # Add order field
