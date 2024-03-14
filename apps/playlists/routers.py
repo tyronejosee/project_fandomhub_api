b765f6a938ca6cdebf5e72059acdc41b00bdd84b
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2024-03-14 18:39:00 -0300

    refactor(playlists): update viewsets and routers

--- apps/playlists/routers.py
+++ apps/playlists/routers.py
@@ -2,10 +2,14 @@
 
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from apps.playlists.viewsets import PlaylistViewSet
+from apps.playlists.viewsets import (
+    PlaylistViewSet, PlaylistAnimeViewSet, PlaylistMangaViewSet
+)
 
 router_v1 = DefaultRouter()
 router_v1.register(r"playlists", PlaylistViewSet, basename="playlist")
+router_v1.register(r"playlist-anime", PlaylistAnimeViewSet, basename="p-anime")
+router_v1.register(r"playlist-manga", PlaylistMangaViewSet, basename="p-anime")
 
 urlpatterns = [
     path("api/v1/", include(router_v1.urls))
--- apps/playlists/viewsets.py
+++ apps/playlists/viewsets.py
@@ -1,30 +1,93 @@
 """Viewsets for Playlists App."""
 
+from django.utils.translation import gettext as _
 from rest_framework import viewsets
+from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from apps.playlists.models import Playlist, PlaylistItem
+from apps.playlists.models import Playlist, PlaylistAnime, PlaylistManga
 from apps.playlists.serializers import (
-    PlaylistSerializer, PlaylistItemSerializer
+    PlaylistSerializer, PlaylistAnimeSerializer, PlaylistMangaSerializer
 )
 
 
 class PlaylistViewSet(viewsets.ModelViewSet):
+    """ViewSet for managing playlists."""
+
     serializer_class = PlaylistSerializer
     ordering = ["id"]
 
     def get_queryset(self):
+        # Return the playlists for the current user
         user = self.request.user
         return Playlist.objects.filter(user=user)
 
+    def perform_create(self, serializer):
+        # Save the playlist with the current user
+        serializer.save(user=self.request.user)
+
     @action(detail=True, methods=["get"], url_path="animes")
     def anime_list(self, request, pk=None):
-        """Pending."""
+        """Get the list of anime in the playlist."""
         playlist = self.get_object()
-        playlists = PlaylistItem.objects.filter(
+        playlists = PlaylistAnime.objects.filter(
             playlist=playlist,
-            anime__isnull=False
         )
-        serializer = PlaylistItemSerializer(playlists, many=True)
-        print(serializer.data)
+        serializer = PlaylistAnimeSerializer(playlists, many=True)
         return Response(serializer.data)
+
+
+class PlaylistAnimeViewSet(viewsets.ModelViewSet):
+    """ViewSet for managing anime in playlists."""
+
+    serializer_class = PlaylistAnimeSerializer
+    ordering = ["id"]    # Add order field
+
+    def get_queryset(self):
+        # Return the anime in the current playlist
+        if hasattr(self, "playlist"):
+            return PlaylistAnime.objects.filter(playlist=self.playlist)
+        user = self.request.user
+        self.playlist = Playlist.objects.get(user=user)
+        return (
+            PlaylistAnime.objects
+            .filter(playlist=self.playlist)
+            .select_related("playlist")
+        )
+
+    @action(detail=True, methods=["post"], url_path="mark-favorite")
+    def switch_is_favorite(self, request, pk=None):
+        """Mark an anime as favorite or not."""
+        playlist_item = self.get_object()
+        playlist_item.is_favorite = not playlist_item.is_favorite
+        playlist_item.save()
+
+        message = (
+            _("Anime marked as favorite.")
+            if playlist_item.is_favorite
+            else _("Anime not marked as favorite.")
+        )
+
+        return Response(
+            {"message": message},
+            status=status.HTTP_200_OK
+        )
+
+
+class PlaylistMangaViewSet(viewsets.ModelViewSet):
+    """ViewSet for managing manga in playlists."""
+
+    serializer_class = PlaylistMangaSerializer
+    ordering = ["id"]    # Add order field
+
+    def get_queryset(self):
+        # Return the manga in the current playlist
+        if hasattr(self, "playlist"):
+            return PlaylistManga.objects.filter(playlist=self.playlist)
+        user = self.request.user
+        self.playlist = Playlist.objects.get(user=user)
+        return (
+            PlaylistManga.objects
+            .filter(playlist=self.playlist)
+            .select_related("playlist")
+        )
