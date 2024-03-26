f5d8a87182598bdc89659a104f1510a97c8c48b9
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2024-03-26 16:39:28 -0300

    feat(news): add serializers, viewsets and routers

---
+++ apps/news/serializers.py
@@ -0,0 +1,36 @@
+"""Serializers for News App."""
+
+from rest_framework import serializers
+# from drf_spectacular.utils import extend_schema_field
+
+from .models import New
+
+
+class NewListSerializer(serializers.ModelSerializer):
+    """Serializer for New model (List only).."""
+    tag = serializers.CharField(source="get_tag_display")
+    author = serializers.SerializerMethodField()
+
+    class Meta:
+        """Meta definition for NewListSerializer."""
+        model = New
+        fields = [
+            "id", "title", "description", "image", "tag", "author"
+        ]
+        read_only_fields = ["author"]
+
+    def get_author(self, obj):
+        return obj.author.username
+
+
+class NewSerializer(serializers.ModelSerializer):
+    """Serializer for New model."""
+    tag = serializers.CharField(source="get_tag_display")
+
+    class Meta:
+        """Meta definition for NewSerializer."""
+        model = New
+        fields = [
+            "id", "title", "description", "content", "image",
+            "source", "tag", "author", "created_at", "updated_at"
+        ]
--- apps/news/viewsets.py
+++ apps/news/viewsets.py
@@ -1,1 +1,40 @@
 """Viewsets for News App."""
+
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_cookie
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
+
+from apps.utils.permissions import IsStaffOrReadOnly
+from .models import New
+from .serializers import NewSerializer, NewListSerializer
+
+
+class NewViewSet(ListModelMixin,
+                 RetrieveModelMixin,
+                 GenericViewSet):
+    """Viewset for managing New instances."""
+    serializer_class = NewSerializer
+    permission_classes = [IsStaffOrReadOnly]
+    search_fields = ["title", "author__name"]
+    ordering_fields = ["author__name"]
+    ordering = ["id"]
+
+    def get_queryset(self):
+        return New.objects.filter(available=True)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return NewListSerializer
+        return super().get_serializer_class()
+
+    @method_decorator(cache_page(60 * 60 * 2))
+    @method_decorator(vary_on_cookie)
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @method_decorator(cache_page(60 * 60 * 2))
+    @method_decorator(vary_on_cookie)
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
