05a4706451681e8d54b188fa60723b534e67e021
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2024-06-14 18:30:18 -0400

    feat(tops): add tops views for manga, characters, artists, and reviews

--- apps/tops/urls.py
+++ apps/tops/urls.py
@@ -2,12 +2,34 @@
 
 from django.urls import path
 
-from .views import TopAnimeView
+from .views import (
+    TopAnimeView,
+    TopMangaView,
+    TopCharacterView,
+    TopArtistView,
+    TopReviewView,
+)
 
 
 urlpatterns = [
     path(
         "api/v1/top/animes/",
         TopAnimeView.as_view(),
+    ),
+    path(
+        "api/v1/top/mangas/",
+        TopMangaView.as_view(),
+    ),
+    path(
+        "api/v1/top/characters/",
+        TopCharacterView.as_view(),
+    ),
+    path(
+        "api/v1/top/artists/",
+        TopArtistView.as_view(),
+    ),
+    path(
+        "api/v1/top/reviews/",
+        TopReviewView.as_view(),
     ),
 ]
--- apps/tops/views.py
+++ apps/tops/views.py
@@ -11,11 +11,20 @@ from rest_framework import status
 
 from apps.animes.models import Anime
 from apps.animes.serializers import AnimeMinimalSerializer
+from apps.mangas.models import Manga
+from apps.mangas.serializers import MangaMinimalSerializer
+from apps.characters.models import Character
+from apps.characters.serializers import CharacterMinimalSerializer
+from apps.persons.models import Person
+from apps.persons.choices import CategoryChoices
+from apps.persons.serializers import PersonMinimalSerializer
+from apps.reviews.models import Review
+from apps.reviews.serializers import ReviewReadSerializer
 
 
 class TopAnimeView(APIView):
     """
-    View lists the 100 most popular animes.
+    View lists the most popular animes.
 
     Endpoints:
     - GET api/v1/top/animes/
@@ -39,7 +48,7 @@ class TopAnimeView(APIView):
                 status=status.HTTP_400_BAD_REQUEST,
             )
 
-        top_animes = Anime.objects.all()
+        top_animes = Anime.objects.get_available()
 
         if type_param:
             top_animes = top_animes.filter(media_type=type_param)
@@ -59,24 +68,80 @@ class TopAnimeView(APIView):
         if top_animes.exists():
             serializer = self.serializer_class(top_animes, many=True)
             return Response(serializer.data)
-        return Response({"detail": _("Empty list.")}, status=status.HTTP_404_NOT_FOUND)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class TopMangaView(APIView):
-    pass
+    """
+    View lists the most popular mangas.
+
+    Endpoints:
+    - GET api/v1/top/mangas/
+    """
+
+    def get(self, request, *args, **kwargs):
+        top_mangas = Manga.objects.get_available().order_by("-favorites")
+        if top_mangas.exists():
+            serializer = MangaMinimalSerializer(top_mangas, many=True)
+            return Response(serializer.data)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    # TODO: Add query params and limit
 
 
 class TopCharacterView(APIView):
-    pass
+    """
+    View lists the most popular characters.
+
+    Endpoints:
+    - GET api/v1/top/characters/
+    """
+
+    def get(self, request, *args, **kwargs):
+        top_characters = Character.objects.get_available().order_by("-favorites")
+        if top_characters.exists():
+            serializer = CharacterMinimalSerializer(top_characters, many=True)
+            return Response(serializer.data)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    # TODO: Add query params and limit
 
 
 class TopArtistView(APIView):
-    pass
+    """
+    View lists the most popular artists.
 
+    Endpoints:
+    - GET api/v1/top/artists/
+    """
 
-class TopAuthorView(APIView):
-    pass
+    def get(self, request, *args, **kwargs):
+        top_artists = (
+            Person.objects.get_available()
+            .filter(category=CategoryChoices.ARTIST)
+            .order_by("-favorites")
+        )
+        if top_artists.exists():
+            serializer = PersonMinimalSerializer(top_artists, many=True)
+            return Response(serializer.data)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    # TODO: Add query params and limit
 
 
 class TopReviewView(APIView):
-    pass
+    """
+    View lists the most popular reviews.
+
+    Endpoints:
+    - GET api/v1/top/reviews/
+    """
+
+    def get(self, request, *args, **kwargs):
+        top_reviews = Review.objects.get_available().order_by("-helpful_count")
+        if top_reviews.exists():
+            serializer = ReviewReadSerializer(top_reviews, many=True)
+            return Response(serializer.data)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    # TODO: Add query params and limit
