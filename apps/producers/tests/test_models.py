a3dfaa598710381579c890bed590003051b3011e
tyronejose <alt.tyronejose@gmail.com>
2024-11-22 19:41:39 -0300

    test(producers): add model tests for producer

---
+++ apps/producers/tests/test_models.py
@@ -0,0 +1,49 @@
+"""Model Tests for Genres App."""
+
+import pytest
+from django.db import IntegrityError
+
+from ..models import Producer
+
+
+@pytest.mark.django_db
+class TestProducerModel:
+    """Tests for Producer model."""
+
+    def test_producer_creation(self, producer):
+        producer = Producer.objects.create(
+            name="Wit Studio",
+            name_jpn="ウィットスタジオ",
+            about=producer.about,
+            established=producer.established,
+            type=producer.type,
+            image=producer.image,
+        )
+        assert producer.name == "Wit Studio"
+        assert str(producer) == "Wit Studio"
+
+    def test_producer_unique_fields(self, producer):
+        with pytest.raises(IntegrityError):
+            Producer.objects.create(
+                name=producer.name,
+                name_jpn=producer.name_jpn,
+                about=producer.about,
+                established=producer.established,
+                type=producer.type,
+                image=producer.image,
+            )
+
+    def test_producer_slug_generation(self, producer):
+        producer = Producer.objects.create(
+            name="Wit Studio",
+            name_jpn="ウィットスタジオ",
+            about=producer.about,
+            established=producer.established,
+            type=producer.type,
+            image=producer.image,
+        )
+        assert producer.slug == "wit-studio"
+
+    def test_get_available_manager(self, producer):
+        results = Producer.objects.get_available()
+        assert results.count() == 1
