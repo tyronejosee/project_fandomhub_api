3d581e8c8c587ac996f63ae4d9ad5187f18d5890
tyronejose <alt.tyronejose@gmail.com>
2024-12-03 20:18:55 -0300

    test(utils): fix picture, video factories, and requeriments file

--- apps/utils/tests/factories.py
+++ apps/utils/tests/factories.py
@@ -1,14 +1,13 @@
 """Factories for Utils App."""
 
 import factory
-from uuid import uuid4
 from django.contrib.contenttypes.models import ContentType
 
 from apps.animes.models import Anime
 from apps.mangas.models import Manga
 from apps.characters.models import Character
 from apps.persons.models import Person
-from ..models import Picture
+from ..models import Picture, Video
 from ..functions import generate_test_image, generate_random_code
 
 
@@ -22,17 +21,38 @@ class PictureFactory(factory.django.DjangoModelFactory):
     content_type = factory.LazyAttribute(
         lambda o: ContentType.objects.get_for_model(o.content_object.__class__)
     )
-    object_id = factory.LazyAttribute(lambda o: uuid4())
+    object_id = factory.LazyAttribute(lambda o: o.content_object.id)
     name = factory.LazyFunction(lambda: generate_random_code())
     image = factory.LazyAttribute(lambda _: generate_test_image(size=(600, 600)))
 
     @factory.lazy_attribute
     def content_object(self):
-        model_mapping = {
-            "anime": Anime.objects.first(),
-            "manga": Manga.objects.first(),
-            "character": Character.objects.first(),
-            "person": Person.objects.first(),
-        }
-        content_type_model = model_mapping.get(self.content_type.model)
-        return content_type_model
+        model_mapping = [
+            Anime.objects.first(),
+            Manga.objects.first(),
+            Character.objects.first(),
+            Person.objects.first(),
+        ]
+        return next((model for model in model_mapping if model is not None), None)
+
+
+class VideoFactory(factory.django.DjangoModelFactory):
+    """Factory for Video model."""
+
+    class Meta:
+        model = Video
+        skip_postgeneration_save = True
+
+    content_type = factory.LazyAttribute(
+        lambda o: ContentType.objects.get_for_model(o.content_object.__class__)
+    )
+    object_id = factory.LazyAttribute(lambda o: o.content_object.id)
+    video = factory.Faker("url")
+
+    @factory.lazy_attribute
+    def content_object(self):
+        model_mapping = [
+            Anime.objects.first(),
+            Manga.objects.first(),
+        ]
+        return next((model for model in model_mapping if model is not None), None)
